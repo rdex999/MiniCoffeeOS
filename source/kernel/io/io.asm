92d0f80c8cb92e5576e68ac38e8217a5bca90de3
rdex999 <didabom2150@gmail.com>
2024-03-31 22:40:35 +0300

    Added comments to printStr

--- source/kernel/io/io.asm
+++ source/kernel/io/io.asm
@@ -62,18 +62,18 @@
 ;   - 1) DS:SI    => Null terminated string
 ; Doesnt return anything
 printStr:
-  mov ax, di
-  mov ah, al
+  mov ax, di                ; Get color in AX
+  mov ah, al                ; Color should be in high 8 bits
 
-  push es
-  push gs
-  mov bx, VGA_SEGMENT
-  mov es, bx
-  mov bx, KERNEL_SEGMENT
-  mov gs, bx
+  push es                   ; Save segments
+  push gs                   ;
+  mov bx, VGA_SEGMENT       ; Set ES to VGA segment (0B8000h) so we write to the VGA memory
+  mov es, bx                ;
+  mov bx, KERNEL_SEGMENT    ; Set GS to kernel segment to read corect trmIndex
+  mov gs, bx                ;
 
-  mov di, gs:[trmIndex]
-  cld
+  mov di, gs:[trmIndex]     ; Get the index of the VGA in DI, as we use ES:DI to write to the VGA
+  cld                       ; Clear direction flag so LODSB and STOSW will increment registers (SI and DI, respectively)
 printStr_loop:
   lodsb                     ; Load character from DS:SI to AL, and increment SI
 
@@ -83,14 +83,14 @@ printStr_loop:
   cmp al, NEWLINE           ; Check if its a newline 
   je printStr_newline       ; If it is then print a newline
 
-  cmp al, CARRIAGE_RETURN
-  je printStr_carriageReturn
+  cmp al, CARRIAGE_RETURN   ; Check if its a carriage return character
+  je printStr_carriageReturn; If it is then handle it
 
-  cmp al, TAB
-  je printStr_tab
+  cmp al, TAB               ; Check if its a tab
+  je printStr_tab           ; If it is then handle it
 
-  stosw
-  jmp printStr_loop
+  stosw                     ; If its not a special character then store AX in ES:DI, and increment DI
+  jmp printStr_loop         ; Continue printing characters
 
 printStr_newline:
   push ax                   ; Save color (in AH)
@@ -104,38 +104,39 @@ printStr_newline:
   jmp printStr_loop         ; Continue printing characters
 
 printStr_carriageReturn:
-  push ax
-  mov ax, di
-  mov bx, 80*2
-  xor dx, dx
-  div bx
-  sub di, dx
-  pop ax
+  push ax                   ; Save color
+  mov ax, di                ; Get index in AX as we divibe it
+  mov bx, 80*2              ; Divibe by the number of columns per column
+  xor dx, dx                ; Zero out remainder
+  div bx                    ; index % 80*2
+  sub di, dx                ; Subtract result from index to get to the start of the line
+  pop ax                    ; Restore color
   jmp printStr_loop         ; Continue printing characters
 
 printStr_tab:
   push ax                   ; Save color
-  mov ax, di                ; Get 
+  mov ax, di                ; Get index in AX as we divibe it
 
-  inc ax
+  inc ax                    ; Increase index so tab will allways have an effect
 
-  mov bx, TXT_TAB_SIZE
-  xor dx, dx
-  div bx
+  ; Closest high dividable number => num * ceil(num / 4)
+  mov bx, TXT_TAB_SIZE      ; Divibe by the tab size
+  xor dx, dx                ; Zero out remainder
+  div bx                    ;
 
-  test dx, dx
-  jz printStr_tab_afterInc
+  test dx, dx               ; Check if the remainder
+  jz printStr_tab_afterInc  ; If the remainder is zero then dont increment result
 
-  inc ax
+  inc ax                    ; If remainder is not zero then increment result
 
 printStr_tab_afterInc:
-  shl ax, 2
-  mov cx, ax 
-  pop ax 
+  shl ax, 2                 ; log2(4) = 2   // Multiply by 4 (TXT_TAB_SIZE)
+  mov cx, ax                ; Get the nearest dividable number in CX
+  pop ax                    ; Restore color
 
-  mov al, ' '
-  sub cx, di
-  repe stosw
+  mov al, ' '               ; Fill the rest of the space with spaces with the color
+  sub cx, di                ; The amount of spaces to print (CX is the high pointer, DI is the low pointer)
+  repe stosw                ; Store AX at ES:DI, increment DI, and repeat until CX is zero
 
   jmp printStr_loop         ; Continue printing characters
   
