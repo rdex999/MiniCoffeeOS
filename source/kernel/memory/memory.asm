fe46c5bb2d0caba0b8a1a1ac054bca4334f84397
rdex999 <didabom2150@gmail.com>
2024-04-18 11:23:04 +0300

    Made memcpy handle overlaps

--- source/kernel/memory/memory.asm
+++ source/kernel/memory/memory.asm
@@ -83,21 +83,73 @@ heapPrintHChunks_end:
 ;   - 1) DS:SI    => Memory to copy data from, the source.
 ;   - 2) DX       => The amount of memory to copy, in bytes.
 ; RETURNS
-;   - This functions return type is void.
+;   - ES:DI       => The original destination pointer
 memcpy:
-  mov cx, dx        ; Move the amount to copy to CX, as it is used as a counter for the REP instruction
-  
-  shr cx, 1         ; Divide amount of bytes to copy by 2, because we will copy it by 2 bytes at a time (word)
-  
-  cld               ; Clear direction flag so MOVSW will increase DI by 2 each time
-  rep movsw         ; Copy 2 bytes from DS:SI to ES:DI, and repeat until CX is 0
+  push bp                   ; Save stack frame
+  mov bp, sp                ;
+  sub sp, 8                 ; Allocate 8 bytes
 
-  test dx, 1        ; Test if the amount of bytes to copy (the parameter) is a multiple of 2 (if its not, then need to copy one more byte)
-  jz memcpy_end     ; If it is, then just return
+  mov [bp - 2], es          ; Store Destination segment
+  mov [bp - 4], di          ; Store destination offset
+  mov [bp - 6], si          ; Store source offset
+  mov [bp - 8], dx          ; Store amount of bytes to copy
 
-  movsb             ; If not a multiple of 2 then copy the last byte and then return
+  ; Check if the destination and the source are overlapping. 
+  ; But first check if their segments are the same, because if not then they are not overlapping
+  mov ax, es                ; Get destination segment in AX
+  mov bx, ds                ; Get source segment in BX
+  cmp ax, bx                ; Check if the segments are the same
+  jne memcpy_afterOverlap   ; If not then just copy normaly
+
+  ; If the segments are the same, then check if the offsets are overlapping
+  mov ax, si                ; Copy source offset to AX
+  add ax, dx                ; Add to it the length (amount of bytes to copy)
+  cmp ax, di                ; Check if its greater than the destination offset
+  jbe memcpy_afterOverlap   ; If not then copy normaly (because they are not overlapping)
+
+  ; If they are overlapping then get the overlap length in AX, copy the overlap to its location then change the size and copy normaly
+  sub ax, di                ; Get the overlap size in AX
+
+  ; Get pointer to the start of the overlap, in SI
+  add si, dx                ; Add to the source offset the size
+  sub si, ax                ; Subtract the length of the overlap, to get the start of the overlap
+
+  ; Get a pointer to the overlaps destinations (where to copy the overlapping bytes to)
+  add di, dx                ; Add to the destination offset the size
+  sub di, ax                ; Subtract the length of the overlap
+
+  ; Copy the overlapping bytes
+  mov cx, ax                ; Amount of bytes to copy
+  shr cx, 1                 ; Divide by 2 because copying 2 bytes at a time
+
+  cld                       ; Clear direction flag so MOVSW will increase DI and SI
+  rep movsw                 ; Copy bytes from DS:SI to ES:DI, increase SI and DI, and repeat until CX == 0
+  test ax, 1                ; Because we divided it by 2, check if the amount of bytes copied is a multiple of 2
+  jz memcpy_prepCopy        ; If it is, then dont copy one more byte
+
+  ; If not a multiple of 2 then copy the last byte
+  movsb                     ; Copy byte from DS:SI to ES:DI
+
+memcpy_prepCopy:
+  mov si, [bp - 6]          ; Get the original source offset
+  mov di, [bp - 4]          ; Get the original destination offset
+  sub dx, ax                ; Subtract the length of the overlap from the size, so we wont mess up the overlap we just fixed
+
+memcpy_afterOverlap:
+  mov cx, dx                ; Amount of bytes to copy in CX
+  shr cx, 1                 ; Divide by 2 because copying 2 bytes each time
+  cld                       ; Clear direction flag so MOVSW will increase SI and DI each time
+  rep movsw                 ; Copy bytes from DS:SI to ES:DI, increase SI and DI, and repeat until CX == 0
+  test dx, 1                ; Check if the amount of bytes copied is a multiple of 2, because we divided it by 2
+  jz memcpy_end             ; If it is then return
+
+  movsb                     ; If not then copy the last byte
 
 memcpy_end:
+  mov es, [bp - 2]          ; Return the destination segment
+  mov di, [bp - 4]          ; Return the destination offset
+  mov sp, bp                ; Restore stack frame
+  pop bp                    ;
   ret
 
 
